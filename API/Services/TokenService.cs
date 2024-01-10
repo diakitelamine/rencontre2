69b56d8817d30da9318853d4411e32cdf79912fd
diakitelamine <diakitelamine555@gmail.com>
2024-01-10 20:54:14 +0100

    hachage

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -30,7 +30,7 @@ public class TokenService : ITokenService
 
         claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
 
-        var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha256Signature);
+        var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
 
         var tokenDescriptor = new SecurityTokenDescriptor
         {
